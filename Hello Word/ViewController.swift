5bf6e8ef28c7742c90bde9aff7815abf16e3b97a
rick <Radik-sidehnko@ukr.net>
2018-02-10 19:28:42 +0200

    add project

---
+++ Hello Word/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Hello Word
+//
+//  Created by Radomyr Sidenko on 10.02.2018.
+//  Copyright © 2018 Radomyr Sidenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var testField: UITextField!
+    var currentNuber: Int = 0
+    
+    var action:String? //var action:String = ""
+
+    @IBAction func celarBut(_ sender: Any) {
+        testField.text = ""
+    }
+    @IBAction func clearLast(_ sender: Any) {
+        testField.text = String (describing:
+         testField.text?.dropLast() ?? "")
+    }
+    
+    
+    @IBAction func equal(_ sender: Any) {
+        if(action == "plus"){
+            testField.text = String (fetcText() + currentNuber)
+        }else if(action == "minus"){
+            testField.text = String (currentNuber - fetcText())
+        }
+        
+    }
+    @IBAction func plus(_ sender: Any) {
+        currentNuber = fetcText()
+        testField.text = ""
+        action = "plus"
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        currentNuber = fetcText()
+        testField.text = ""
+        action = "minus"
+    }
+    
+    func fetcText() -> Int{
+        if let currentTitle = testField.text,
+            let number = Int (currentTitle){
+            return number
+        }
+        return 0
+    }
+    
+    @IBAction func buttonPresed1(_ sender: UIButton!){
+        let currentDigit = sender.currentTitle
+        
+        if let currentTitle = testField.text{
+            let appendDigit = currentTitle + currentDigit!
+            testField.text = appendDigit
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
